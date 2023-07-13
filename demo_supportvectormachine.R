86928b5859ae56d7c6c406ab30a17c6d21de9f72
Manh Huy Nguyen <huynguyenmanh150894@gmail.com>
2023-07-13 09:06:33 -0400

    test

--- demo_supportvectormachine.R
+++ demo_supportvectormachine.R
@@ -1,6 +1,7 @@
 library(e1071)
 library(caret)
 library(ROCR)
+library(MLmetrics)
 
 my.data <- shared_comp_plastic_type  %>%
   dplyr::select(File, collapsed_compound, Percent_Area) %>%
@@ -27,17 +28,16 @@ for (c in 2:ncol(filled.data)) {
 }
 
 # split train and test data 60:40 , random sampling is done within the levels of plastic_type
-samp <- caret::createDataPartition(as.factor(filled.data$plastic_type), p = 0.5, list = F)
+samp <- caret::createDataPartition(filled.data$plastic_type, p = 0.5, list = F)
 train <- filled.data[samp,]
 test <- filled.data[-samp,]
 
-# Train and Select best cost parameter
+# Train and Select best cost parameter -------------------
 tune.out <- tune(svm, plastic_type ~ ., data = train, kernel = "polynomial",
                  ranges = list(cost = c(0.001 , 0.01 , 0.1 , 1, 5, 10, 100)),
                                # gamma = c(0.5 , 1, 2, 3, 4)),
                  decision.values = TRUE)
 # summary(tune.out)
-
 bestmod <- tune.out$best.model
 View(table(bestmod$fitted, train$plastic_type))
 
@@ -46,5 +46,39 @@ ypred <- predict(bestmod, test)
 
 View(table(ypred, test$plastic_type))
 
+# Caret: Control params for SVM
+ctrl <- caret::trainControl(
+  method = "cv", 
+  number = 10, 
+  classProbs = TRUE,                 
+  summaryFunction = multiClassSummary  # also needed for AUC/ROC
+)
+
+# Tune an SVM with caret
+svm_auc <- caret::train(
+  plastic_type ~ ., 
+  data = train,
+  method = "svmPoly",               
+  preProcess = c("center", "scale"),
+  metric = "ROC",
+  trControl = ctrl,
+  tuneLength = 10
+)
+
+# Print results
+svm_auc$results
+
+caret::confusionMatrix(svm_auc)
 
 
+
+# Feature importance
+prob_yes <- function(object, newdata) {
+  predict(object, newdata = newdata, type = "prob")[, "Yes"]
+}
+
+# Variable importance plot
+set.seed(2827)  # for reproducibility
+vip(bestmod, method = "permute", nsim = 5, train = train, 
+    target = "plastic_type", metric = "auc", reference_class = "Yes", 
+    pred_wrapper = prob_yes)
